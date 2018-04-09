492d6c14f682f1acbe18ac4da1d51fe30310badc
Kartik Bhalla <kartikbhalla53@yahoo.com>
2018-04-09 05:25:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     flag = "Fizz"
-  end
   elsif int % 5 == 0
     flag = "Buzz"
-  end
   elsif int % 15 == 0
     flag = "FizzBuzz"
-  end
   return flag
 end
